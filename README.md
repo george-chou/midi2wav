b25be6c354703a2bc72443c09d2135ecbcd0ad1b
George <20459298+george-chou@users.noreply.github.com>
2023-03-04 13:47:44 +0800

    upl all codes

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # midi2wav
+
+A Python script for rendering MIDI files into WAV audios on x64 Windows 10
+
+## Usage
+
+1. Install <a href="https://www.123pan.com/s/Hl2SVv-EbWRh.html" target="_blank">fluidsynth-2.2.4-win10-x64</a>;
+
+2. Download <a href="https://www.123pan.com/s/Hl2SVv-kbWRh.html" target="_blank">soundfonts</a> into `./sf` directory;
+
+3. Run the following command as an example:
+```
+python render.py --sf "./sf/JV1080 Nice Piano_m.sf2"
+```
---
+++ render.py
@@ -0,0 +1,30 @@
+import os
+import argparse
+from midi2audio import FluidSynth
+
+parser = argparse.ArgumentParser(description='soundfont')
+parser.add_argument(
+    '--sf',
+    type=str,
+    default='./sf/Splendid.136.sf2',
+    help='Select a sound font.'
+)
+args = parser.parse_args()
+
+
+def render(midi_dir, audio_dir, font):
+    fs = FluidSynth(sound_font=font)
+    for _, _, filenames in os.walk(midi_dir):
+        for filename in filenames:
+            input = midi_dir + '/' + filename
+            fontname = font.replace("\\", "/").split('/')[-1][:-4]
+            output = audio_dir + '/' + filename[:-4] + '(' + fontname + ').wav'
+            print(input, output)
+            fs.midi_to_audio(input, output)
+
+
+if __name__ == "__main__":
+    midi_dir = './input'
+    sound_font = args.sf
+    audio_dir = './output'
+    render(midi_dir, audio_dir, sound_font)
