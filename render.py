64028fa805604781e6e7dff66d8d0422976b3f25
George <20459298+george-chou@users.noreply.github.com>
2023-03-05 11:55:36 +0800

    fix render sf unknown arg bug

--- render.py
+++ render.py
@@ -17,17 +17,16 @@ def mkdir(outpath='./output'):
         os.makedirs(outpath)
 
 
-def render(midi_dir, audio_dir, font):
-    fs = FluidSynth(sound_font=font)
+def render(midi_dir, audio_dir, sf):
+    fs = FluidSynth(sound_font=sf)
     for _, _, filenames in os.walk(midi_dir):
         for filename in filenames:
             input = midi_dir + '/' + filename
-            fontname = font.replace("\\", "/").split('/')[-1][:-4]
+            fontname = sf.replace("\\", "/").split('/')[-1][:-4]
             output = audio_dir + '/' + filename[:-4] + '(' + fontname + ').wav'
             mkdir(audio_dir)
             fs.midi_to_audio(input, output)
 
 
 if __name__ == "__main__":
-
-    render(midi_dir='./input', audio_dir='./output', sound_font=args.sf)
+    render(midi_dir='./input', audio_dir='./output', sf=args.sf)
